9b748e1af407691cf3c5cb6dcf27bcd811df7892
sparklymina <ecausevic93@gmail.com>
2021-09-20 07:25:27 -0400

    adding ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,10 @@
+// Write code below this ling
+    function functionPrintInfo(name, age){
+        console.log(name, age)
+    }
+    functionPrintInfo("John", 23);
+    functionPrintInfo("Michael", 43);
+
+// Write code above this line
+
+module.exports = functionPrintInfo;
