6e9960aa4d5cfbbbee26e4e2cafd5894ed0e641e
sparklymina <ecausevic93@gmail.com>
2021-09-20 12:59:45 -0400

    adding ex14

---
+++ ex14/src/index.js
@@ -0,0 +1,10 @@
+// Only change code below this line
+function convertStrToInt(str) {
+    var str = parseInt(str);
+    return str; // IS THIS WHAT SHOULD BE RETURNED?
+}
+console.log(convertStrToInt(44));
+console.log(convertStrToInt(27));
+console.log(convertStrToInt("JeffBezos"));
+// Only change code above this line
+module.exports = convertStrToInt;
