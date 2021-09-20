7e5853558b7d598ade1fde3aec162854e95d2bf6
sparklymina <ecausevic93@gmail.com>
2021-09-20 09:57:50 -0400

    adding ex07

---
+++ ex07/src/index.js
@@ -0,0 +1,20 @@
+// Only change code below this line
+var sum = 0
+function addThree() {
+    sum = sum + 3;
+}
+addThree();
+
+function addFive() {
+    sum = sum + 5;
+}
+addFive();
+// Only change code above this line
+
+module.exports = [
+    addThree,
+    addFive
+];
+
+
+// DOESN'T WORK!!!!!!
